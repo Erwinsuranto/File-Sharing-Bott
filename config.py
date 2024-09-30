f88d2947c36fd2eb8661cd146901d29ae74dc8b4
Erwinsuranto <erwinsuranto00@gmail.com>
2024-10-01 01:26:35 +0800

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ from logging.handlers import RotatingFileHandler
 load_dotenv("config.env")
 
 # Bot token dari @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7020443800:AAFIADB5b2pVFY80Pwx_8MvJqviZl9wzKiQ")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7604489139:AAFY9DYkhvRIbRNIKh5W63sRtZgVUBkUGRc")
 
 # API ID Anda dari my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "25448292"))
@@ -20,7 +20,7 @@ APP_ID = int(os.environ.get("APP_ID", "25448292"))
 API_HASH = os.environ.get("API_HASH", "abdfbb83e9442cbbf94e976d8ec5c486")
 
 # ID Channel Database
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "1002267714791"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002267714791"))
 
 # NAMA OWNER
 OWNER = os.environ.get("OWNER", "7568265690")
@@ -36,11 +36,11 @@ HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
 UPSTREAM_BRANCH = os.environ.get("UPSTREAM_BRANCH", "master")
 
 # Database
-DB_URI = os.environ.get("DATABASE_URL", "")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://rossy:rossy@cluster0.hx7fm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "1002274212670"))
-FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "1002428554532"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002200609780"))
+FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-1002424398717"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
